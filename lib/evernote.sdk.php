754e925fa66292fa34ae71645f32d4f30c10da65
Makis Tracend <makis.tracend@gmail.com>
2014-01-27 23:11:15 -0800

    loading proper dependencies as per latest SDK

--- lib/evernote.sdk.php
+++ lib/evernote.sdk.php
@@ -5,6 +5,8 @@
 $GLOBALS['THRIFT_ROOT'] = $evernote_sdk = SDK. "evernote/1.25/";
 
 require_once($evernote_sdk ."autoload.php");
+require_once($evernote_sdk ."Evernote/Client.php");
+/*
 require_once($evernote_sdk ."Thrift.php");
 require_once($evernote_sdk ."transport/TTransport.php");
 require_once($evernote_sdk ."transport/THttpClient.php");
@@ -16,13 +18,12 @@ require_once($evernote_sdk ."packages/UserStore/UserStore.php");
 require_once($evernote_sdk ."packages/UserStore/UserStore_constants.php");
 require_once($evernote_sdk ."packages/NoteStore/NoteStore.php");
 require_once($evernote_sdk ."packages/Limits/Limits_constants.php");
-
+*/
 
 // Import the classes that we're going to be using
 #use EDAM\UserStore\UserStoreClient;
 #use EDAM\NoteStore\NoteStoreClient;
 #use EDAM\Types\Data, EDAM\Types\Note, EDAM\Types\Resource, EDAM\Types\ResourceAttributes;
 #use EDAM\Error\EDAMSystemException, EDAM\Error\EDAMUserException, EDAM\Error\EDAMErrorCode;
-
 
 ?>
